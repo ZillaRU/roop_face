9dadf7717a173c0f5b5f3e117bd92292863e5ad6
ZillaRU <zilla_ru@zju.edu.cn>
2024-08-27 22:03:34 +0800

    check file exists

--- download.sh
+++ download.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ ! -d "bmodel_files" ]; then
-    wget https://github.com/ZillaRU/roop_face/releases/download/v0.1/bmodel_files.tar.gz
-    tar -xzf bmodel_files.tar.gz
+    if [ ! -f "bmodel_files.tar.gz" ]; then
+        wget https://github.com/ZillaRU/roop_face/releases/download/v0.1/bmodel_files.tar.gz
+    fi
+    tar xzf bmodel_files.tar.gz
     rm -rf bmodel_files.tar.gz
 fi
